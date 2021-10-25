6ba4ab8bdd569f6db3fb8e73ba29713bb1bff9de
agan <agan@toimc.com>
2021-10-25 11:57:51 +0800

    upgrade vue/cli to 4

--- src/utils/errorHandle.js
+++ src/utils/errorHandle.js
@@ -1,5 +1,6 @@
-const errorHandle = (err) => {
-  console.log(err)
-}
 
+/* eslint-disable handle-callback-err */
+const errorHandle = (err) => {
+// console.log("🚀 ~ file: errorHandle.js ~ line 4 ~ errorHandle ~ err", err)
+}
 export default errorHandle
